8fc90aa2b9be83b33ab99e0fe9a32d4d788f1140
John U <arjorb@gmail.com>
2023-02-27 12:02:17 +0200

    done creating the state for the mode

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
-  const [mode, setMode] = useState(false);
+  const [mode, setMode] = useState(true);
 
   const style = {
     backgroundColor: `${mode ? '#ffffff' : '#282D35'}`,
     color: `${mode ? '#282D35' : '#ffffff'}`,
   };
+
+  const handleMode = () => {
+    setMode(prevMode => !prevMode);
+    console.log(mode);
+  };
   return (
     <div className='flex justify-center mt-20'>
       <div className='w-[550px]' style={style}>
-        <Header />
-        <Hero />
+        <Header mode={mode} handleMode={handleMode} />
+        <Hero mode={mode} />
       </div>
     </div>
   );
