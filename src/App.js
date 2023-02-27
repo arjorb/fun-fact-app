e4728821750df55e87b2f95cc913dd06c2ef48fb
John U <arjorb@gmail.com>
2023-02-27 11:58:30 +0200

    adding the icon for the hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
+  const [mode, setMode] = useState(false);
+
+  const style = {
+    backgroundColor: `${mode ? '#ffffff' : '#282D35'}`,
+    color: `${mode ? '#282D35' : '#ffffff'}`,
+  };
   return (
     <div className='flex justify-center mt-20'>
-      <div className='w-[550px] bg-white '>
+      <div className='w-[550px]' style={style}>
         <Header />
         <Hero />
       </div>
